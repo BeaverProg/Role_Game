8cbf6343db75090e4b3ed79d571d7c7273ee3884
BeaverProg <egor.bobrov@icloud.com>
2024-12-07 02:16:06 +0300

    У мира появились свои статусы Теперь есть 2 магазина В них можно покупать товары и выходить из магазина

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Hero:
         self.hp = 100
         self.atk = 10
         self.df = 10
-        self.money_counter = 0
+        self.money_counter = 20
 
         self.img = cv2.imread('images/wizard.png')
         self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
@@ -25,7 +25,7 @@ class Hero:
 
         img2 = cv2.putText(img2, 'Health: ' + str(main_char.hp), (190 * 5 + 25, 100),
                                  cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3, cv2.LINE_AA)
-        img2 = cv2.putText(img2, 'Strength: ' + str(main_char.atk), (190 * 5 + 25, 100 + 60),
+        img2 = cv2.putText(img2, 'Attack: ' + str(main_char.atk), (190 * 5 + 25, 100 + 60),
                                  cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3, cv2.LINE_AA)
         img2 = cv2.putText(img2, 'Defence: ' + str(main_char.df), (190 * 5 + 25, 100 + 60 + 60),
                            cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3, cv2.LINE_AA)
@@ -41,14 +41,14 @@ class Hero:
             if prise == 1:
                 self.hp -= 30
             else:
-                self.money_counter += 1
+                self.money_counter += 2
         else:
             if prise == 1:
                 self.hp -= 30
             elif prise == 2 or prise == 3:
                 self.hp += 10
             else:
-                self.money_counter += 1
+                self.money_counter += 2
 
 
 class Chest:
@@ -69,7 +69,8 @@ class Chest:
             chest_x = random.choice(line_x)
             chest_y = random.choice(line_y)
 
-            while chest_x == char.x and chest_y == char.y:
+            while ((chest_x == char.x and chest_y == char.y) or
+                   (chest_x == 5 and chest_y == 489) or (chest_x == 766 and chest_y == 5)):
                 chest_x = random.choice(line_x)
                 chest_y = random.choice(line_y)
 
@@ -90,6 +91,7 @@ class Craft:
 
         self.x = 766
         self.y = 5
+
         self.img = cv2.imread('images/craft.png')
         self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
 
@@ -107,19 +109,129 @@ class Craft:
 
         if 'stick' in self.products:
             img2 = draw_character(img2, self.stick_img, 200, 70)
-            img2 = cv2.putText(img2, 'Attack: +10 ', (200, 150 + 480 - 20),
+
+            if world_status == 'craft':
+                label1 = 'Attack: +10'
+                label2 = 'Cost: 10'
+            else:
+                label1 = 'Health: +20'
+                label2 = 'Cost: 10'
+
+            img2 = cv2.putText(img2, label1, (200, 150 + 480 - 20),
                                cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3, cv2.LINE_AA)
-            img2 = cv2.putText(img2, 'Cost: 10 ', (200, 150 + 480 + 40),
+            img2 = cv2.putText(img2, label2, (200, 150 + 480 + 40),
                                cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3, cv2.LINE_AA)
 
         if 'shield' in self.products:
             img2 = draw_character(img2, self.shield_img, 750, 100)
-            img2 = cv2.putText(img2, 'Defence: +10 ', (750, 150 + 480 - 20),
+
+            if world_status == 'craft':
+                label1 = 'Defence: +10'
+                label2 = 'Cost: 10'
+            else:
+                label1 = 'Atack: +20'
+                label2 = 'Cost: 20'
+
+            img2 = cv2.putText(img2, label1, (750, 150 + 480 - 20),
                                cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3, cv2.LINE_AA)
-            img2 = cv2.putText(img2, 'Cost: 10 ', (750, 150 + 480 + 40),
+            img2 = cv2.putText(img2, label2, (750, 150 + 480 + 40),
                                cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 3, cv2.LINE_AA)
 
         return img2
+
+    def buy(self, results, char: 'Hero', img2):
+
+        y_top = int(results.multi_hand_landmarks[0].landmark[4].y *
+                    flippedRGB.shape[0])
+        x_top = int(results.multi_hand_landmarks[0].landmark[4].x *
+                    flippedRGB.shape[0])
+        y_low = int(results.multi_hand_landmarks[0].landmark[8].y *
+                    flippedRGB.shape[0])
+        x_low = int(results.multi_hand_landmarks[0].landmark[8].x *
+                    flippedRGB.shape[0])
+
+        check_finger = abs(y_top - y_low) <= 30 and abs(x_low - x_top) <= 30
+
+        if check_finger and 130 <= x_top <= 230 and 100 <= y_top <= 530 and 'stick' in self.products:
+
+            if char.money_counter < 5:
+
+                img2 = cv2.putText(img2, 'Not enough money ', (670, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                                   1.5, (0, 255, 0), 3, cv2.LINE_AA)
+
+            else:
+                self.products.pop(self.products.index('stick'))
+                char.money_counter -= 5
+                if world_status == 'craft':
+                    char.atk += 10
+                else:
+                    char.hp += 20
+
+        elif check_finger and 430 <= x_top <= 620 and 150 <= y_top <= 560 and 'shield' in self.products:
+
+            if char.money_counter < 5 or char.money_counter < 20 and world_status == 'alch':
+
+                img2 = cv2.putText(img2, 'Not enough money ', (670, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                                   1.5, (0, 255, 0), 3, cv2.LINE_AA)
+
+            else:
+                self.products.pop(self.products.index('shield'))
+                char.money_counter -= 5
+                if world_status == 'craft':
+                    char.df += 10
+                else:
+                    char.atk += 30
+                    char.money_counter -= 15
+
+        return img2
+
+    def exit_button(self, results, img2):
+
+        y_top = int(results.multi_hand_landmarks[0].landmark[4].y *
+                    flippedRGB.shape[0])
+        x_top = int(results.multi_hand_landmarks[0].landmark[4].x *
+                    flippedRGB.shape[0])
+        y_low = int(results.multi_hand_landmarks[0].landmark[8].y *
+                    flippedRGB.shape[0])
+        x_low = int(results.multi_hand_landmarks[0].landmark[8].x *
+                    flippedRGB.shape[0])
+
+        check_finger = abs(y_top - y_low) <= 30 and abs(x_low - x_top) <= 30
+
+        img2 = cv2.putText(img2, 'Exit the shop', (340, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                           1.5, (0, 255, 0), 3, cv2.LINE_AA)
+
+        if check_finger and 200 <= x_low <= 370 and 0 <= y_low <= 50:
+            return 'main'
+        else:
+            return 'craft'
+
+
+class Alchemi(Craft):
+
+    def __init__(self):
+
+        self.img = cv2.imread('images/alch.png')
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.x = 5
+        self.y = 539
+
+        self.products = ['stick', 'shield']
+
+        self.stick_img = cv2.imread('images/health_potion.png')
+        self.stick_img = cv2.cvtColor(self.stick_img, cv2.COLOR_BGR2RGB)
+        self.stick_img = cv2.resize(self.stick_img, (198, 480))
+
+        self.shield_img = cv2.imread('images/bomb.png')
+        self.shield_img = cv2.cvtColor(self.shield_img, cv2.COLOR_BGR2RGB)
+        self.shield_img = cv2.resize(self.shield_img, (368, 448))
+
+    def exit_button(self, results, img2):
+        ans = super().exit_button(results, img2)
+        if ans == 'craft':
+            ans = 'alch'
+
+        return ans
 
 
 def touch(ch: 'Chest', mn: 'Hero') -> None:
@@ -243,6 +355,7 @@ cap = cv2.VideoCapture(0)
 main_char = Hero()
 chest = Chest()
 craft = Craft()
+alch = Alchemi()
 field_plane = [[[5, 5], [196, 5], [387, 5], [578, 5], [766, 5]],
                [[5, 245], [196, 245], [387, 245], [578, 245], [766, 245]],
                [[5, 489], [196, 489], [387, 489], [578, 489], [766, 489]]]
@@ -268,6 +381,7 @@ while cap.isOpened():
         draw_field()
 
         flippedRGB = draw_character(flippedRGB, craft.img, craft.x, craft.y)
+        flippedRGB = draw_character(flippedRGB, alch.img, alch.x, alch.y)
 
         flippedRGB = main_char.draw_text(flippedRGB)
 
@@ -283,10 +397,36 @@ while cap.isOpened():
 
         if craft.x == main_char.x and craft.y == main_char.y:
             world_status = 'craft'
+            main_char.goal_x = 578
+            main_char.goal_y = 5
+        elif alch.x == main_char.x and alch.y == main_char.y + 50:
+            world_status = 'alch'
+            main_char.goal_x = 5
+            main_char.goal_y = 245
 
     elif world_status == 'craft':
 
         craft.draw_items(flippedRGB)
+
+        if results.multi_hand_landmarks is not None:
+
+            craft.buy(results, main_char, flippedRGB)
+
+            world_status = craft.exit_button(results, flippedRGB)
+
+            cursor_draw()
+
+    elif world_status == 'alch':
+
+        alch.draw_items(flippedRGB)
+
+        if results.multi_hand_landmarks is not None:
+
+            alch.buy(results, main_char, flippedRGB)
+
+            world_status = alch.exit_button(results, flippedRGB)
+
+            cursor_draw()
 
     res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
     cv2.imshow("Game", res_image)
